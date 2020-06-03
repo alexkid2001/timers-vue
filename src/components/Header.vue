5c083fcaf3aecbdac9ccc024863d4b4b9802c52e
naydyonov <naydyonov@etm-system.ru>
2020-06-04 00:37:59 +0300

    mobile menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,15 @@
   .header
     .container.header__wrap
       .header__logo AppStarter
-      .header__mobile-menu-btn
+      .header__mobile-menu-btn(
+        @click="openMobileMenu"
+        v-click-outside="closeMobileMenu"
+      )
         svg(style="width:24px;height:24px" viewBox="0 0 24 24")
           path(d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z")
-      .header__nav 
+      .header__nav(
+        :class="isMenuOpen ? 'open' : ''"
+      ) 
         ul.nav
           li.nav__item Features
           li.nav__item Video Tour
@@ -21,8 +26,32 @@
 import FormButton from './FormButton.vue'
 
 export default {
+  data() {
+    return {
+      isMenuOpen: false
+    }
+  },
   components: {
     FormButton
+  },
+  methods: {
+    openMobileMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+      this.bodyOverlay();
+    },
+    closeMobileMenu() {
+      this.isMenuOpen = false;
+      this.bodyOverlay();
+    },
+    bodyOverlay() {
+      const body = document.querySelector('body');
+      if (this.isMenuOpen) {
+        body.classList.add('overlay');
+      } else {
+        body.classList.remove('overlay');
+      }
+    }
+
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import './assets/scss/main.scss';
 
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      // here I check that click was outside the el and his childrens
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  },
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
